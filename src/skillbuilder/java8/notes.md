b8354dd8390871a687eb21b059b99bab1ab3ff79
LAPTOP-HDA61V4N\Ankit Shivane <ankitshivane.as@gmail.com>
2025-10-20 18:04:02 +0530

    updated the set1

--- src/skillbuilder/java8/notes.md
+++
@@ -1,4 +0,0 @@
-## Coding Notes
-* if we have a string "abc" and wanted to process each and every character of it one by one:
-  * then str.chars().mapToObj(i->(char)i)
-  * then Arrays.stream(str.split(""))
--- src/skillbuilder/java8/set1/CheckIfStringAnagram.java
+++ src/skillbuilder/java8/set1/CheckIfStringAnagram.java
@@ -1,7 +1,13 @@
 package skillbuilder.java8.set1;
 
+import skillbuilder.indepth.sealed_classes.subpkg1.Account;
+
+import java.sql.Struct;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,31 +15,36 @@ public class CheckIfStringAnagram {
 
     public static void main(String[] args) {
 
-        /*
+        /**
          * Java 8 program to check if two strings are anagrams or not?
+         * Concept: Two strings are anagrams if they contain the same characters with the same frequency, regardless of order.
+         * Example:
+         * listen → silent ✅
+         * triangle → integral ✅
+         * apple → papel ✅
+         * rat → car ❌
          */
-        String s1 = "RaceCkar";
-        String s2 = "CarRace";
-        approach1(s1, s2);
-        System.out.println(areAnagrams(s1, s2));//false // core java approach
-    }
+        String s1 = "listen";
+        String s2 = "silent";
+        /**
+         * Below three solutions are based on java stream
+         */
+        System.out.println("Approach 1:->" + isAnagram(s1, s2));
+        System.out.println("Approach 2:->" + isAnagramApproach2(s1, s2));
+        System.out.println("Approach 3:->" + isAnagramApproach3(s1, s2));
 
-    private static void approach1(String s1, String s2) {
-        //get sorted string using approach 1
-        String s1Modified = s1.chars().mapToObj(i -> (char) i).sorted(Comparator.naturalOrder()).map(String::valueOf)
-                .collect(Collectors.joining());
-        System.out.println("S1 after sorting:" + s1Modified);
-        //get sorted string using approach 2
-        String s2Approach2 = Stream.of(s2.split("")).sorted().peek(i -> System.out.print(i))
-                .collect(Collectors.joining());
-        System.out.println();
-        if (s1Modified.equalsIgnoreCase(s2Approach2)) {
-            System.out.println("Both are anagram of each other");
-        } else {
-            System.out.println("Not an anagram");
-        }
-    }
+        // Core Java approach
+        System.out.println("Approach 4:->" + areAnagrams(s1, s2));//false // core java approach
+        System.out.println("Approach 5:->" + approach5(s1, s2));//false // core java approach
+        /** OUTPUT
+         Approach 1:->true
+         Approach 2:->true
+         Approach 3:->true
+         Approach 4:->true
+         Approach 5:->true
 
+         */
+    }
 
     public static boolean areAnagrams(String str1, String str2) {
         // Null or empty string handling
@@ -57,9 +68,79 @@ public class CheckIfStringAnagram {
         // Sort the character arrays
         Arrays.sort(charArray1);
         Arrays.sort(charArray2);
-
         // Compare the sorted arrays
         return Arrays.equals(charArray1, charArray2);
+        /*
+        Time Complexity: O(n log n)
+        Space: O(n)
+         */
     }
 
+    static boolean isAnagram(String s1, String s2) {
+        return normalize(s1).equals(normalize(s2));
+//         Internal Working:
+//        chars() → IntStream of Unicode values
+//        sorted() → sorts them
+//        mapToObj() → converts back to String
+//        joining() → merges into single String
+//
+//        Pros: Clean and expressive
+//        Cons: O(n log n) due to sorting
+    }
+
+    private static String normalize(String s) {
+        return s.toLowerCase().replaceAll("\\s+", "")
+                .chars()
+                .sorted()
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+    }
+
+    static boolean isAnagramApproach2(String s1, String s2) {
+        return getFreqCount(s1).equals(getFreqCount(s2));
+//        Pros: No sorting (O(n))
+//        Cons: Slight overhead of map creation
+    }
+
+    //
+    private static Map<Character, Long> getFreqCount(String s1) {
+        return s1.toLowerCase().replaceAll("\\s+", "")
+                .chars()
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
+    }
+
+    private static boolean isAnagramApproach3(String s1, String s2) {
+        return Arrays.equals(
+                getSortedArray(s1),
+                getSortedArray(s2)
+        );
+    }
+
+    private static int[] getSortedArray(String s1) {
+        return s1.toLowerCase().replaceAll("\\s+", "")
+                .chars()
+                .sorted()
+                .toArray();
+    }
+
+    private static boolean approach5(String s1, String s2) {
+        Map<Character, Integer> counter = new LinkedHashMap<>();
+        for (char c : s1.toCharArray()) {
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
+        }
+        for (char c : s2.toCharArray()) {
+            if (!counter.containsKey(c)) {
+                return false;
+            }
+
+            counter.put(c, counter.get(c) - 1);
+            if (counter.get(c) == 0) {
+                counter.remove(c);
+            }
+        }
+        return true;
+
+    }
 }
